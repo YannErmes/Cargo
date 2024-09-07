7d487cb0e9275f2fb5216675c409a380d5a1adac
yann <ermes1643@gmail.com>
2024-09-07 02:23:20 +0200

    first commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3409638593"
+    serviceWorkerVersion: "1752108913"
   }
 });
